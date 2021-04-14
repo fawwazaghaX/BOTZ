96c9e27040de6317aea91770ae77ad1cc05efd1a
Naylachan <82255308+naylachan@users.noreply.github.com>
2021-04-15 05:44:40 +0800

    Add files via upload

---
+++ reply/nyz.js
@@ -0,0 +1,400 @@
+exports.menuZ = (prefix, bulan, tchat, ownername) => {
+	return`╔════════════════════
+║╭═══════════════════
+║│➵ \`\`\`BOT WHATSAPP NEW\`\`\`
+║│➵ \`\`\`OWNER : ${ownername}\`\`\`
+║│➵ \`\`\`AUTHOR : NAYLA CHAN\`\`\`
+║│➵ \`\`\`NAMEBOT : BOTWA\`\`\`
+║│➵ \`\`\`BULAN : ${bulan}\`\`\`
+║│➵ \`\`\`CHAT : ${tchat}\`\`\`
+║╰═══════════════════─
+╠════[ \`\`\`ANTI•MENU\`\`\` ]══════
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}antilink\`\`\`
+║│➵ \`\`\`${prefix}antigay\`\`\`
+║│➵ \`\`\`${prefix}antibocil\`\`\`
+║│➵ \`\`\`${prefix}antiwibu\`\`\`
+║│➵ \`\`\`${prefix}antijawa\`\`\`
+║╰═══════════════════─
+╠════[ \`\`\`MENU•MEDIA\`\`\` ]══════
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}fb\`\`\`
+║│➵ \`\`\`${prefix}steam\`\`\`
+║│➵ \`\`\`${prefix}igfoto\`\`\`
+║│➵ \`\`\`${prefix}igvideo\`\`\`
+║│➵ \`\`\`${prefix}ytsearch\`\`\`
+║│➵ \`\`\`${prefix}ytmp3\`\`\`
+║│➵ \`\`\`${prefix}ytmp4\`\`\`
+║│➵ \`\`\`${prefix}play\`\`\`
+║│➵ \`\`\`${prefix}stalktwit\`\`\`
+║│➵ \`\`\`${prefix}stalkgithub\`\`\`
+║╰═══════════════════
+╠════[ \`\`\`FUN•MENU\`\`\` ]══════
+║╭═══════════════════
+║│➵ \`\`\`\`\`\`${prefix}simi\`\`\`
+║│➵ \`\`\`${prefix}hilih\`\`\`
+║│➵ \`\`\`${prefix}jumlah\`\`\`
+║│➵ \`\`\`${prefix}hurufkebalik\`\`\`
+║│➵ \`\`\`${prefix}spamcall\`\`\`
+║│➵ \`\`\`${prefix}spamemail\`\`\`
+║│➵ \`\`\`${prefix}fakta\`\`\`
+║│➵ \`\`\`${prefix}gcard\`\`\`
+║│➵ \`\`\`${prefix}quotes\`\`\`
+║│➵ \`\`\`${prefix}tebakgambar\`\`\`
+║╰═══════════════════─
+╠════[ \`\`\`INFO•MENU\`\`\` ]═════
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}neko\`\`\`
+║│➵ \`\`\`${prefix}quotesnime\`\`\`
+║│➵ \`\`\`${prefix}kbbilazimedia\`\`\`
+║│➵ \`\`\`${prefix}covid\`\`\`
+║│➵ \`\`\`${prefix}wikiid\`\`\`
+║│➵ \`\`\`${prefix}wikien\`\`\`
+║│➵ \`\`\`${prefix}covidid\`\`\`
+║│➵ \`\`\`${prefix}kbbi\`\`\`
+║│➵ \`\`\`${prefix}infogempa\`\`\`
+║│➵ \`\`\`${prefix}randomquran\`\`\`
+║│➵ \`\`\`${prefix}kisanabi\`\`\`
+║╰═══════════════════─
+╠════[ \`\`\`MAKER•MENU\`\`\` ]═════
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}8bit\`\`\`
+║│➵ \`\`\`${prefix}burn\`\`\`
+║│➵ \`\`\`${prefix}fire\`\`\`
+║│➵ \`\`\`${prefix}google\`\`\`
+║│➵ \`\`\`${prefix}battle\`\`\`
+║│➵ \`\`\`${prefix}block\`\`\`
+║│➵ \`\`\`${prefix}candy\`\`\`
+║│➵ \`\`\`${prefix}potter\`\`\`
+║│➵ \`\`\`${prefix}silk\`\`\`
+║│➵ \`\`\`${prefix}water\`\`\`
+║│➵ \`\`\`${prefix}pubg\`\`\`
+║│➵ \`\`\`${prefix}neon\`\`\`
+║│➵ \`\`\`${prefix}coffe1\`\`\`
+║│➵ \`\`\`${prefix}coffe\`\`\`
+║│➵ \`\`\`${prefix}tiktok\`\`\`
+║│➵ \`\`\`${prefix}shadow\`\`\`
+║│➵ \`\`\`${prefix}romance\`\`\`
+║│➵ \`\`\`${prefix}glass\`\`\`
+║│➵ \`\`\`${prefix}naruto\`\`\`
+║│➵ \`\`\`${prefix}mug1\`\`\`
+║│➵ \`\`\`${prefix}love\`\`\`
+║│➵ \`\`\`${prefix}mug\`\`\`
+║│➵ \`\`\`${prefix}neon1\`\`\`
+║╰═══════════════════─
+╠═══[ \`\`\`SIMPLE•MENU\`\`\` ]═════
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}artinama\`\`\`
+║│➵ \`\`\`${prefix}artimimpi\`\`\`
+║│➵ \`\`\`${prefix}artijadian\`\`\`
+║│➵ \`\`\`${prefix}chord\`\`\`
+║│➵ \`\`\`${prefix}lirik\`\`\`
+║╰═══════════════════─
+╠══[ \`\`\`SERTIFIKAT•MENU\`\`\` ]═══
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}ff1\`\`\`
+║│➵ \`\`\`${prefix}ff2\`\`\`
+║│➵ \`\`\`${prefix}ff3\`\`\`
+║│➵ \`\`\`${prefix}ff4\`\`\`
+║│➵ \`\`\`${prefix}ff5\`\`\`
+║│➵ \`\`\`${prefix}ff6\`\`\`
+║│➵ \`\`\`${prefix}ml1\`\`\`
+║│➵ \`\`\`${prefix}ml2\`\`\`
+║│➵ \`\`\`${prefix}ml3\`\`\`
+║│➵ \`\`\`${prefix}ml4\`\`\`
+║│➵ \`\`\`${prefix}ml5\`\`\`
+║│➵ \`\`\`${prefix}hacker\`\`\`
+║│➵ \`\`\`${prefix}yutub\`\`\`
+║│➵ \`\`\`${prefix}jamet\`\`\`
+║│➵ \`\`\`${prefix}fuckboy\`\`\`
+║│➵ \`\`\`${prefix}goodlooking\`\`\`
+║│➵ \`\`\`${prefix}editor\`\`\`
+║╰═══════════════════─
+╠══[ \`\`\`SERTIFIKAT2•MENU\`\`\` ]══
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}goodboy\`\`\`
+║│➵ \`\`\`${prefix}goodgirl\`\`\`
+║│➵ \`\`\`${prefix}badgirl\`\`\`
+║│➵ \`\`\`${prefix}badboy\`\`\`
+║│➵ \`\`\`${prefix}pintar\`\`\`
+║│➵ \`\`\`${prefix}surga\`\`\`
+║│➵ \`\`\`${prefix}sadboy\`\`\`
+║│➵ \`\`\`${prefix}pacar\`\`\`
+║│➵ \`\`\`${prefix}gay\`\`\`
+║│➵ \`\`\`${prefix}bucin\`\`\`
+║│➵ \`\`\`${prefix}bocil\`\`\`
+║│➵ \`\`\`${prefix}babu\`\`\`
+║│➵ \`\`\`${prefix}haram\`\`\`
+║│➵ \`\`\`${prefix}tweet\`\`\`
+║│➵ \`\`\`${prefix}pubg1\`\`\`
+║│➵ \`\`\`${prefix}pubg2\`\`\`
+║│➵ \`\`\`${prefix}pubg3\`\`\`
+║│➵ \`\`\`${prefix}pubg4\`\`\`
+║╰═══════════════════─
+╠═══[ \`\`\`TAMBAHAN•MENU\`\`\` ]═══
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}sticker\`\`\`
+║│➵ \`\`\`${prefix}stickergif\`\`\`
+║│➵ \`\`\`${prefix}bc\`\`\`
+║│➵ \`\`\`${prefix}clearall\`\`\`
+║│➵ \`\`\`${prefix}linkgc\`\`\`
+║│➵ \`\`\`${prefix}tagall\`\`\`
+║│➵ \`\`\`${prefix}delete\`\`\`
+║│➵ \`\`\`${prefix}ssweb\`\`\`
+║│➵ \`\`\`${prefix}smoke\`\`\`
+║│➵ \`\`\`${prefix}rainbow\`\`\`
+║╰═══════════════════─
+╠═══[ \`\`\`RANDOM1•MENU\`\`\` ]═══
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}twich\`\`\`
+║│➵ \`\`\`${prefix}loli\`\`\`
+║│➵ \`\`\`${prefix}randomhusbu\`\`\`
+║│➵ \`\`\`${prefix}fake\`\`\`
+║│➵ \`\`\`${prefix}pinterest\`\`\`
+║│➵ \`\`\`${prefix}qrcode\`\`\`
+║│➵ \`\`\`${prefix}resepmasakan\`\`\`
+║│➵ \`\`\`${prefix}barcode\`\`\`
+║│➵ \`\`\`${prefix}cersex\`\`\`
+║│➵ \`\`\`${prefix}cerpen\`\`\`
+║│➵ \`\`\`${prefix}anime\`\`\`
+║│➵ \`\`\`${prefix}anime1\`\`\`
+║│➵ \`\`\`${prefix}yuri\`\`\`
+║│➵ \`\`\`${prefix}tongue\`\`\`
+║│➵ \`\`\`${prefix}jadwalsholat\`\`\`
+║│➵ \`\`\`${prefix}pantun\`\`\`
+║│➵ \`\`\`${prefix}cuaca\`\`\`
+║│➵ \`\`\`${prefix}namaninja\`\`\`
+║╰═══════════════════─
+╠═══[ \`\`\`RANDOM2•MENU\`\`\` ]═══
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}randomwaifu\`\`\`
+║│➵ \`\`\`${prefix}randomwaifu1\`\`\`
+║│➵ \`\`\`${prefix}neko1\`\`\`
+║│➵ \`\`\`${prefix}kusonime\`\`\`
+║│➵ \`\`\`${prefix}renungan\`\`\`
+║│➵ \`\`\`${prefix}samehadaku\`\`\`
+║│➵ \`\`\`${prefix}neon1\`\`\`
+║│➵ \`\`\`${prefix}text3d\`\`\`
+║│➵ \`\`\`${prefix}galaxy\`\`\`
+║│➵ \`\`\`${prefix}gaming\`\`\`
+║│➵ \`\`\`${prefix}colors\`\`\`
+║│➵ \`\`\`${prefix}kling\`\`\`
+║│➵ \`\`\`${prefix}infonomer\`\`\`
+║│➵ \`\`\`${prefix}jadwaltv\`\`\`
+║│➵ \`\`\`${prefix}tvjadwal\`\`\`
+║│➵ \`\`\`${prefix}fml\`\`\`
+║│➵ \`\`\`${prefix}cinta\`\`\`
+║╰═══════════════════─
+╠═══[ \`\`\`RANDOM3•MENU\`\`\` ]═══
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}stalkig\`\`\`
+║│➵ \`\`\`${prefix}estetikpic\`\`\`
+║│➵ \`\`\`${prefix}memeindo\`\`\`
+║│➵ \`\`\`${prefix}darkjokes\`\`\`
+║│➵ \`\`\`${prefix}nickff\`\`\`
+║│➵ \`\`\`${prefix}tahta\`\`\`
+║│➵ \`\`\`${prefix}neon2\`\`\`
+║│➵ \`\`\`${prefix}wall\`\`\`
+║│➵ \`\`\`${prefix}wolf\`\`\`
+║│➵ \`\`\`${prefix}tfire\`\`\`
+║│➵ \`\`\`${prefix}ytgold\`\`\`
+║│➵ \`\`\`${prefix}ytsilver\`\`\`
+║│➵ \`\`\`${prefix}t3d\`\`\`
+║│➵ \`\`\`${prefix}logoa\`\`\`
+║│➵ \`\`\`${prefix}pornhub\`\`\`
+║│➵ \`\`\`${prefix}marvel\`\`\`
+║│➵ \`\`\`${prefix}leavest\`\`\`
+║│➵ \`\`\`${prefix}phcoment\`\`\`
+║│➵ \`\`\`${prefix}nulis\`\`\`
+║│➵ \`\`\`${prefix}urlshort\`\`\`
+║│➵ \`\`\`${prefix}shortener\`\`\`
+║╰═══════════════════─
+╠═══[ \`\`\`RANDOM3•MENU\`\`\` ]═══
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}fox\`\`\`
+║│➵ \`\`\`${prefix}dog\`\`\`
+║│➵ \`\`\`${prefix}cat\`\`\`
+║│➵ \`\`\`${prefix}panda\`\`\`
+║│➵ \`\`\`${prefix}panda1\`\`\`
+║│➵ \`\`\`${prefix}bird\`\`\`
+║│➵ \`\`\`${prefix}koala\`\`\`
+║│➵ \`\`\`${prefix}meme\`\`\` 
+║│➵ \`\`\`${prefix}kapankah\`\`\`
+║│➵ \`\`\`${prefix}apakah\`\`\` 
+║╰═══════════════════─
+╠═══[ \`\`\`INDOHOT•MENU\`\`\` ]═══
+║╭═══════════════════─
+║│➵ \`\`\`${prefix}indo1\`\`\` 
+║│➵ \`\`\`${prefix}indo2\`\`\` 
+║│➵ \`\`\`${prefix}indo3\`\`\` 
+║│➵ \`\`\`${prefix}indo4\`\`\` 
+║│➵ \`\`\`${prefix}indo5\`\`\` 
+║│➵ \`\`\`${prefix}indo6\`\`\` 
+║│➵ \`\`\`${prefix}indo7\`\`\` 
+║│➵ \`\`\`${prefix}indo8\`\`\` 
+║│➵ \`\`\`${prefix}indo9\`\`\` 
+║│➵ \`\`\`${prefix}indo10\`\`\` 
+║│➵ \`\`\`${prefix}indo11\`\`\` 
+║│➵ \`\`\`${prefix}indo12\`\`\` 
+║│➵ \`\`\`${prefix}indo13\`\`\` 
+║│➵ \`\`\`${prefix}indo14\`\`\` 
+║│➵ \`\`\`${prefix}indo15\`\`\` 
+║│➵ \`\`\`${prefix}indo16\`\`\` 
+║│➵ \`\`\`${prefix}indo17\`\`\` 
+║│➵ \`\`\`${prefix}indo18\`\`\` 
+║│➵ \`\`\`${prefix}indo19\`\`\` 
+║│➵ \`\`\`${prefix}indo20\`\`\` 
+║│➵ \`\`\`${prefix}indo21\`\`\` 
+║│➵ \`\`\`${prefix}indo22\`\`\` 
+║│➵ \`\`\`${prefix}indo23\`\`\` 
+║│➵ \`\`\`${prefix}indo24\`\`\` 
+║│➵ \`\`\`${prefix}indo25\`\`\` 
+║╰═══════════════════
+╠═════[ \`\`\`INFO•BOT\`\`\` ]═══════
+║╭═══════════════════
+║│➵ \`\`\`${prefix}ganteng\`\`\`
+║│➵ \`\`\`${prefix}cantik\`\`\`
+║│➵ \`\`\`${prefix}jelek\`\`\`
+║│➵ \`\`\`${prefix}goblok\`\`\`
+║│➵ \`\`\`${prefix}bego\`\`\`
+║│➵ \`\`\`${prefix}pinter\`\`\`
+║│➵ \`\`\`${prefix}jago\`\`\`
+║│➵ \`\`\`${prefix}babi\`\`\`
+║│➵ \`\`\`${prefix}beban\`\`\`
+║│➵ \`\`\`${prefix}baik\`\`\`
+║│➵ \`\`\`${prefix}jahat\`\`\`
+║│➵ \`\`\`${prefix}anjing\`\`\`
+║│➵ \`\`\`${prefix}monyet\`\`\`
+║│➵ \`\`\`${prefix}haram\`\`\`
+║│➵ \`\`\`${prefix}kontol\`\`\`
+║│➵ \`\`\`${prefix}pakboy\`\`\`
+║│➵ \`\`\`${prefix}pakgirl\`\`\`
+║│➵ \`\`\`${prefix}sadboy\`\`\`
+║│➵ \`\`\`${prefix}sadgirl\`\`\`
+║│➵ \`\`\`${prefix}wibu\`\`\`
+║│➵ \`\`\`${prefix}nolep\`\`\`
+║│➵ \`\`\`${prefix}hebat\`\`\`
+║╰═══════════════════
+╠═════[ \`\`\`WALL•MENU\`\`\` ]═══
+║╭═══════════════════
+║│➵ \`\`\`${prefix}wallteknologi\`\`\`
+║│➵ \`\`\`${prefix}wallhacker\`\`\`
+║│➵ \`\`\`${prefix}wallcyber\`\`\`
+║│➵ \`\`\`${prefix}wallmuslim\`\`\`
+║│➵ \`\`\`${prefix}wallpegunungan\`\`\`
+║│➵ \`\`\`${prefix}caklontong\`\`\`
+║│➵ \`\`\`${prefix}robot\`\`\`
+║│➵ \`\`\`${prefix}3dwhite\`\`\`
+║│➵ \`\`\`${prefix}daun\`\`\`
+║│➵ \`\`\`${prefix}metal1\`\`\`
+║│➵ \`\`\`${prefix}metal\`\`\`
+║│➵ \`\`\`${prefix}scary\`\`\`
+║│➵ \`\`\`${prefix}imo\`\`\`
+║│➵ \`\`\`${prefix}wallpaper\`\`\`
+║│➵ \`\`\`${prefix}quotes2\`\`\`
+║│➵ \`\`\`${prefix}quotes1\`\`\`
+║╰═══════════════════
+╠════[ \`\`\`STORAGE•MENU\`\`\` ]═══
+║╭═══════════════════
+║│➵ \`\`\`${prefix}toimg\`\`\`
+║│➵ \`\`\`${prefix}readmore\`\`\`
+║│➵ \`\`\`${prefix}chatlist\`\`\`
+║│➵ \`\`\`${prefix}addsticker\`\`\`
+║│➵ \`\`\`${prefix}addvn\`\`\`
+║│➵ \`\`\`${prefix}getvn\`\`\`
+║│➵ \`\`\`${prefix}getsticker\`\`\`
+║│➵ \`\`\`${prefix}liststicker\`\`\`
+║│➵ \`\`\`${prefix}listvn\`\`\`
+║│➵ \`\`\`${prefix}addimage\`\`\`
+║│➵ \`\`\`${prefix}getimage\`\`\`
+║│➵ \`\`\`${prefix}imagelist\`\`\`
+║│➵ \`\`\`${prefix}addvideo\`\`\`
+║│➵ \`\`\`${prefix}getvideo\`\`\`
+║│➵ \`\`\`${prefix}listvideo\`\`\`
+║╰═══════════════════
+╠═══[ \`\`\`SOUND•MENU\`\`\` ]═══
+║╭═══════════════════
+║│➵ \`\`\`${prefix}sound1\`\`\`
+║│➵ \`\`\`${prefix}sound2\`\`\`
+║│➵ \`\`\`${prefix}sound3\`\`\`
+║│➵ \`\`\`${prefix}sound4\`\`\`
+║│➵ \`\`\`${prefix}sound5\`\`\`
+║│➵ \`\`\`${prefix}sound6\`\`\`
+║│➵ \`\`\`${prefix}sound7\`\`\`
+║│➵ \`\`\`${prefix}sound8\`\`\`
+║│➵ \`\`\`${prefix}sound9\`\`\`
+║│➵ \`\`\`${prefix}sound10\`\`\`
+║│➵ \`\`\`${prefix}sound11\`\`\`
+║│➵ \`\`\`${prefix}sound12\`\`\`
+║│➵ \`\`\`${prefix}sound13\`\`\`
+║│➵ \`\`\`${prefix}sound14\`\`\`
+║│➵ \`\`\`${prefix}sound15\`\`\`
+║│➵ \`\`\`${prefix}sound16\`\`\`
+║│➵ \`\`\`${prefix}sound17\`\`\`
+║│➵ \`\`\`${prefix}sound18\`\`\`
+║│➵ \`\`\`${prefix}sound19\`\`\`
+║│➵ \`\`\`${prefix}sound20\`\`\`
+║│➵ \`\`\`${prefix}sound21\`\`\`
+║│➵ \`\`\`${prefix}sound22\`\`\`
+║│➵ \`\`\`${prefix}sound23\`\`\`
+║│➵ \`\`\`${prefix}sound24\`\`\`
+║╰═══════════════════
+╠═[ \`\`\`RANDOM•XTEAM\`\`\` ]═══
+║╭═══════════════════
+║│➵ \`\`\`${prefix}asupan\`\`\`
+║│➵ \`\`\`${prefix}asupan1\`\`\`
+║│➵ \`\`\`${prefix}asupan2\`\`\`
+║│➵ \`\`\`${prefix}ngakak\`\`\`
+║│➵ \`\`\`${prefix}nulis1\`\`\`
+║│➵ \`\`\`${prefix}nulis2\`\`\`
+║│➵ \`\`\`${prefix}nulis3\`\`\`
+║│➵ \`\`\`${prefix}nulis4\`\`\`
+║│➵ \`\`\`${prefix}nulis5\`\`\`
+║│➵ \`\`\`${prefix}nulis6\`\`\`
+║│➵ \`\`\`${prefix}video1\`\`\`
+║│➵ \`\`\`${prefix}video2\`\`\`
+║│➵ \`\`\`${prefix}video3\`\`\`
+║│➵ \`\`\`${prefix}video4\`\`\`
+║│➵ \`\`\`${prefix}video5\`\`\`
+║│➵ \`\`\`${prefix}video6\`\`\`
+║╰═══════════════════
+╠═[ \`\`\`YAMETE•KUDASAI\`\`\` ]══
+║╭═══════════════════
+║│➵ \`\`\`${prefix}orgy\`\`\`                     
+║│➵ \`\`\`${prefix}nsfwneko\`\`\`
+║│➵ \`\`\`${prefix}mstrb\`\`\` 
+║│➵ \`\`\`${prefix}manga\`\`\` 
+║│➵ \`\`\`${prefix}jahy\`\`\` 
+║│➵ \`\`\`${prefix}hentaigif\`\`\` 
+║│➵ \`\`\`${prefix}hentai\`\`\` 
+║│➵ \`\`\`${prefix}glasses\`\`\`
+║│➵ \`\`\`${prefix}gangbang\`\`\` 
+║│➵ \`\`\`${prefix}ero\`\`\` 
+║│➵ \`\`\`${prefix}cuckold\`\`\` 
+║│➵ \`\`\`${prefix}blowjob?\`\`\` 
+║│➵ \`\`\`${prefix}bdsm\`\`\` 
+║│➵ \`\`\`${prefix}ass\`\`\` 
+║│➵ \`\`\`${prefix}ahegao\`\`\` 
+║│➵ \`\`\`${prefix}zettairyouiki\`\`\` 
+║│➵ \`\`\`${prefix}wpnsfwmobile\`\`\` 
+║│➵ \`\`\`${prefix}wpmobile\`\`\` 
+║│➵ \`\`\`${prefix}wallpaper\`\`\`
+║│➵ \`\`\`${prefix}thighs\`\`\` 
+║│➵ \`\`\`${prefix}uniform\`\`\`
+║│➵ \`\`\`${prefix}tentacles\`\`\` 
+║│➵ \`\`\`${prefix}sfwneko\`\`\` 
+║│➵ \`\`\`${prefix}pussy\`\`\` 
+║│➵ \`\`\`${prefix}panties\`\`\` 
+║╰═══════════════════
+╠══════[ \`\`\`THX TO\`\`\` ]═══════
+║╭═══════════════════
+║│➵ \`\`\`NAYLA CHAN\`\`\`
+║│➵ \`\`\`RYNZ\`\`\`
+║│➵ \`\`\`ARA•ARA TEAM\`\`\` 
+║│\`\`\`github.com/naylachan\`\`\`
+║╰═══════════════════
+╚════════════════════`
+}
+exports.botx = (prefix) => {
+	return`[❗] MODE BOTX TIDAK AKTIF\nKETIK *${prefix}botx*\nUNTUK MENGAKTIFKAN`
+	}
